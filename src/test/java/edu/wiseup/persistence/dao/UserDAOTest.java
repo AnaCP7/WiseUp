91a19da2db2651d2e1d5a6e2136cee133cea9c74
ana.capilla.perez <anacapper24@gmail.com>
2023-05-31 12:34:24 +0200

    UserDAOTest añadido, tests passed: 1/1

--- src/test/java/edu/wiseup/persistence/dao/UserDAOTest.java
+++ src/test/java/edu/wiseup/persistence/dao/UserDAOTest.java
@@ -1,4 +1,26 @@
 package edu.wiseup.persistence.dao;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserDAOTest {
+
+    @Test
+    public void testUserDAOConstructor() throws SQLException {
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.when(resultSet.getInt("id")).thenReturn(1);
+        Mockito.when(resultSet.getString("username")).thenReturn("testuser");
+        Mockito.when(resultSet.getString("password")).thenReturn("password");
+
+        UserDAO userDAO = new UserDAO(resultSet);
+
+        assertEquals(1, userDAO.getId());
+        assertEquals("testuser", userDAO.getUsername());
+        assertEquals("password", userDAO.getPassword());
+    }
 }
