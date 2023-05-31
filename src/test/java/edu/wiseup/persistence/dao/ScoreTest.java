8f16ca1f22a775aa992487909bf3eacb4451d0f3
ana.capilla.perez <anacapper24@gmail.com>
2023-05-31 12:36:33 +0200

    ScoreTest añadido, tests passed: 1/1

--- src/test/java/edu/wiseup/persistence/dao/ScoreTest.java
+++ src/test/java/edu/wiseup/persistence/dao/ScoreTest.java
@@ -1,4 +1,41 @@
 package edu.wiseup.persistence.dao;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ScoreTest {
+
+    @Mock
+    private ResultSet resultSetMock;
+
+    @InjectMocks
+    private Score score;
+
+    @Test
+    public void testScoreConstructor() throws SQLException {
+        int id = 1;
+        int scoreValue = 100;
+        String dateStr = "2023-05-30T12:34:56.00Z";
+
+        when(resultSetMock.getInt("id")).thenReturn(id);
+        when(resultSetMock.getInt("score")).thenReturn(scoreValue);
+        when(resultSetMock.getString("date")).thenReturn(dateStr);
+
+        score = new Score(resultSetMock);
+
+        assertEquals(id, score.getId());
+        assertEquals(scoreValue, score.getScore());
+        assertEquals(Instant.parse(dateStr), score.getDate());
+    }
 }
